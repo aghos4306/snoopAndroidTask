b7ecb5bd5f7fee69354ac1558f2347bae76610e5
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2024-07-11 22:32:17 +0100

    updated the design of renderd cat and data in catlist and catdetailed screen

--- app/src/main/java/com/aghogho/snoopandroidtask/presentation/cat_detail/CatDetailScreen.kt
+++ app/src/main/java/com/aghogho/snoopandroidtask/presentation/cat_detail/CatDetailScreen.kt
@@ -1,25 +1,42 @@
 package com.aghogho.snoopandroidtask.presentation.cat_detail
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import coil.compose.rememberAsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CatDetailScreen(
     navController: NavController,
@@ -32,66 +49,124 @@ fun CatDetailScreen(
         catDetailViewModel.getCatDetailInfo(id)
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        if (catDetailState.isLoading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-        } else if (catDetailState.catDetailData != null) {
-            val catData = catDetailState.catDetailData
-            Column {
-                Text(
-                    text = catData.name,
-                    style = MaterialTheme.typography.headlineMedium
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Image(
-                    modifier = Modifier
-                        .size(200.dp)
-                        .align(Alignment.CenterHorizontally),
-                    painter = rememberAsyncImagePainter(model = catData.imageUrl),
-                    contentDescription = catData.name
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = "Cat Origin: ${catData.origin}",
-                    style = MaterialTheme.typography.bodyMedium
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = "Description: ${catData.description}",
-                    style = MaterialTheme.typography.bodyMedium
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = "Temperament: ${catData.temperament}",
-                    style = MaterialTheme.typography.bodyMedium
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = "Life Span: ${catData.lifeSpan}",
-                    style = MaterialTheme.typography.bodyMedium
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = "More info: ${catData.wikipediaUrl}",
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-        } else if (catDetailState.error.isNotBlank()) {
-            Text(
-                text = catDetailState.error,
-                color = MaterialTheme.colorScheme.error,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.align(Alignment.Center)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = catDetailState.catDetailData?.name ?: "Cat Detail",
+                        modifier = Modifier.fillMaxWidth(),
+                        style = MaterialTheme.typography.headlineMedium.copy(
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Back",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
             )
+        },
+        content = { paddingValues ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(16.dp)
+            ) {
+                if (catDetailState.isLoading) {
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                } else if (catDetailState.catDetailData != null) {
+                    val catData = catDetailState.catDetailData
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .size(200.dp)
+                                .clip(CircleShape)
+                                .background(Color.Gray)
+                                .align(Alignment.CenterHorizontally)
+                        ) {
+                            SubcomposeAsyncImage(
+                                model = catData.imageUrl,
+                                contentDescription = catData.name,
+                                modifier = Modifier
+                                    .size(200.dp)
+                                    .clip(CircleShape),
+                                contentScale = ContentScale.Crop
+                            ) {
+                                SubcomposeAsyncImageContent()
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = "Cat Origin: ${catData.origin}",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        Text(
+                            text = "Description",
+                            color = MaterialTheme.colorScheme.primary,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = catData.description,
+                            style = MaterialTheme.typography.bodyMedium,
+                        )
+                        
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        Text(
+                            text = "Temperament",
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                        Text(
+                            text = catData.temperament,
+                            style = MaterialTheme.typography.bodyMedium,
+                            textAlign = TextAlign.Center,
+                        )
+
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = "Life Span: ${catData.lifeSpan} years",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        Text(
+                            text = "More info",
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                        Text(
+                            text = catData.wikipediaUrl,
+                            style = MaterialTheme.typography.bodyMedium,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                } else if (catDetailState.error.isNotBlank()) {
+                    Text(
+                        text = catDetailState.error,
+                        color = MaterialTheme.colorScheme.error,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+            }
         }
-    }
+    )
 }
-
-
 
 
 
--- app/src/main/java/com/aghogho/snoopandroidtask/presentation/cat_list/components/CatListItem.kt
+++ app/src/main/java/com/aghogho/snoopandroidtask/presentation/cat_list/components/CatListItem.kt
@@ -1,7 +1,9 @@
 package com.aghogho.snoopandroidtask.presentation.cat_list.components
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
@@ -17,11 +20,16 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
+import androidx.compose.ui.unit.sp
+import coil.compose.SubcomposeAsyncImage
 import com.aghogho.snoopandroidtask.domain.model.CatModel
-
 @Composable
 fun CatListItem(
     catItems: CatModel,
@@ -32,21 +40,46 @@ fun CatListItem(
         modifier = Modifier
             .fillMaxWidth()
             .clickable { catItems.referenceImageId?.let { onItemClick(it) } }
-            .padding(20.dp)
+            .padding(20.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         catItems.image?.url?.let { imageUrl ->
-            Image(
-                painter = rememberAsyncImagePainter(model = imageUrl),
+            SubcomposeAsyncImage(
+                model = imageUrl,
                 contentDescription = catItems.name,
-                modifier = Modifier.size(72.dp)
+                loading = {
+                    Box(
+                        modifier = Modifier
+                            .size(72.dp)
+                            .clip(CircleShape)
+                            .background(Color.Gray)
+                    )
+                },
+                modifier = Modifier
+                    .size(72.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
             )
         }
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
-            Text(text = catItems.name, style = MaterialTheme.typography.headlineSmall)
+        Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = catItems.name,
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            )
             Text(text = catItems.origin, style = MaterialTheme.typography.bodyMedium)
         }
-        IconButton(onClick = { onFavouriteClicked(catItems) }) {
+        IconButton(
+            onClick = { onFavouriteClicked(catItems) },
+            modifier = Modifier.align(Alignment.CenterVertically)
+        ) {
             Icon(
                 imageVector = if (catItems.isFavourited) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
                 contentDescription = "favorite"
